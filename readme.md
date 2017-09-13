6ddfd7f06c59e9f562f560320256e6ec103065a7
Gleidson da Silva Balcazar <52308090278@tce.govrn>
2017-09-13 18:22:12 -0300

    Inicio da Arquitetura

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Indexador de Vafas - Web Scraping with NodeJs
+
+Exemplo de WebScraping com NodeJs, usando engine hbs
+
+### Author
+
+Gleidson Balcazar.
+
---
+++ src/app.js
@@ -0,0 +1,16 @@
+import path from 'path'
+import express from 'express'
+
+
+
+const app= express()
+const ENV = process.env.NODE_ENV || 'development'
+const envPath = path.join(__dirname, './configs/env/' + ENV)
+
+require(envPath)(app)
+require('./routes/')(app)
+
+
+app.listen(app.get('port'), ()=> {
+  console.log('express has ben started');
+})
