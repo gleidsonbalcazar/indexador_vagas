1554fb08d5c49c9ece03f6bba23540b53863424b
Gleidson da Silva Balcazar <52308090278@tce.govrn>
2017-09-26 16:59:36 -0300

    Customizando header

--- src/routes/landing/index.js
+++ src/routes/landing/index.js
@@ -1,7 +1,7 @@
-import express from 'express'
+import express from 'express';
 import landing from './../../services/landing/';
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', landing)
+router.get('/', landing);
 
 module.exports = router;
